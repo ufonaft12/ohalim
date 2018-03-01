e810b544de45b5d4d5d41e5f8c2a893daccc101d
x_pozytron_x <stetsenko.vitaliy@gmail.com>
2018-03-01 12:17:35 +0200

    правки, куча всего

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 $('.has_submenu').hover( 
 	function(){
 		$('.menu-item_submenu').addClass('submenu-active');
-		$('.has_submenu').css({
-			'border-bottom': '4px solid blue'
-		});
+ 
 	}, function(){
 	$('.menu-item_submenu').removeClass('submenu-active');
-			$('.has_submenu').css({
-			'border-bottom': 'none'
-		});
+ 
 }
 );
 
